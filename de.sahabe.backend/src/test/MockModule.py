fe6fe8085453da199d1fe78ebad7b300e19ec892
maano <abo.habib@gmx.de>
2014-06-25 01:49:35 +0200

    modify MockModule for new database version

--- de.sahabe.backend/src/test/MockModule.py
+++ de.sahabe.backend/src/test/MockModule.py
@@ -11,7 +11,7 @@ import datetime
 
 
 def randomName(size=32, chars=string.ascii_lowercase):
-    return ''.join(random.choice(chars) for _ in range(random.randint(8, size)))
+    return ''.join(random.choice(chars) for _ in range(random.randint(5, size)))
 
 def randomEmail(size=32): 
     return randomText(size - 10) + "@sahabe.de"
@@ -29,21 +29,27 @@ def timeStamp():
 
 class DBMock():
     """
-    user, category, tag, link, tag_map, pw_hash 
+    user, link, search_table, tag, link_tag_map, link_group, link_group_map, meta_data, pw_hash
     """
     user = ""
-    category = ""
-    tag = ""
     link = ""
-    tagMap = ""
+    searchTable = ""
+    tag = ""
+    linkTagMap = ""
+    group = ""
+    linkGroupMap =""
+    metaData = ""
     pw = ""
     
     def __init__(self):
         self.user = DBMock.User()
-        self.category = DBMock.Category(self.user)
-        self.tag = DBMock.Tag(self.user)
-        self.link = DBMock.Link(self.user, self.category)
-        self.tagMap = DBMock.TagMap(self.link, self.tag)
+        self.link = DBMock.Link(self.user)
+        self.searchTable = DBMock.SearchTable(self.user, self.link)
+        self.tag = DBMock.Tag()
+        self.linkTagMap = DBMock.LinkTagMap(self.tag, self.link)
+        self.group = DBMock.Group()
+        self.linkGroupMap = DBMock.LinkGroupMap(self.group, self.link)
+        self.metaData = DBMock.MetaData(self.link)
         self.pw = DBMock.PWHash(self.user)
     
     class User(object):
@@ -55,50 +61,76 @@ class DBMock():
             self.name = randomName()
             self.email = self.name + "@sahabe.de"
     
-    class Category():
-        id = ""
-        userId = ""
-        name = ""
-        def __init__(self, user):
-            self.id = uuid()
-            self.userId = user.id
-            self.name = randomName()
-    
     class Link():
         id = ""
         userId = ""
-        catId = ""
         url = ""
-        name = ""
+        title = ""
         description = ""
-        createDate = ""
-        def __init__(self, user, category):
-            self.id = uuid()
-            self.userId = user.id
-            self.catId = category.id
-            self.url = randomText(64)
-            self.name = randomName()
-            self.desciption = randomText(36)
-            """ make datetime stamp """
-            self.createDate = timeStamp()
-    
-    class Tag():
-        id = ""
-        userId = ""
-        name = ""
-        groupTag = '0'
+        typeName = ""
+        modifiedAt = ""
         def __init__(self, user):
             self.id = uuid()
             self.userId = user.id
-            self.name = randomName()
-            self.groupTag = '0'
-    
-    class TagMap():
+            self.url = randomText(64)
+            self.title = randomName()
+            self.desciption = randomText(32)
+            self.typeName = randomName()
+            self.modifiedAt = timeStamp()
+            
+    class SearchTable():
+        userId = ""
         linkId = ""
-        tagId = ""
-        def __init__(self, link, tag):
+        groups = ""
+        tags = ""
+        text = ""
+        def __init__(self, user, link):
+            self.userId = user.id
             self.linkId = link.id
+            self.groups = randomText(64)
+            self.tags = randomText(64)
+            self.text = randomText(64)
+            
+    class Tag():
+        id = ""
+        name = ""
+        def __init__(self):
+            self.id = uuid()
+            self.name = randomName()
+    
+    class LinkTagMap():
+        tagId = ""
+        linkId = ""
+        def __init__(self, tag, link):
             self.tagId = tag.id
+            self.linkId = link.id
+            
+    class Group():
+        id = ""
+        name = ""
+        public = 0
+        def __init__(self):
+            self.id = uuid()
+            self.name = randomName()      
+            self.public = 0
+            
+    class LinkGroupMap():
+        groupId = ""
+        linkId = ""
+        def __init__(self, group, link):
+            self.groupId = group.id
+            self.linkId = link.id
+            
+    class MetaData():
+        id = ""
+        linkId = ""
+        key = ""
+        value = ""       
+        def __init__(self, link):   
+            self.id = uuid()
+            self.linkId = link.id
+            self.key = randomName()
+            self.value = randomName()
     
     class PWHash():
         userId = "" 
